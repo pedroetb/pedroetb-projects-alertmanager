3cefc0971c5f2087077c7121711639b8dcedd6c4
Pedro Trujillo <pedroetb@gmail.com>
2019-11-30 00:10:18 +0000

    Fix configuration name and variable substitution

--- deploy/config/entrypoint.sh
+++ deploy/config/entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-cat /etc/alertmanager/config-tmpl.yml \
+cat /etc/alertmanager/alertmanager-tmpl.yml \
 	| sed "s@\${SLACK_URL}@${SLACK_URL}@g" \
 	| sed "s@\${SLACK_CHANNEL}@${SLACK_CHANNEL}@g" \
 	| sed "s@\${SLACK_USER}@${SLACK_USER}@g" \
-	> /etc/alertmanager/config.yml
-
-mv /tmp/alertmanager.yml /etc/alertmanager/alertmanager.yml
+	> /etc/alertmanager/alertmanager.yml
 
 set -- /bin/alertmanager "${@}"
 
