7854b49f271f94d8664edaa5a3b6ae2ed63d8f4d
Pedro Trujillo <pedroetb@gmail.com>
2025-08-11 22:56:29 +0100

    Migrate from slack to telegram, add new options
    Drop slack receiver, add telegram receiver instead. Adapt environment
    variables and config files to telegram usage.
    
    Set message template with detailed alert info, added as a new config
    file.
    
    Add port and log level configuration variables.
    
    Update readme.

--- deploy/config/entrypoint.sh
+++ deploy/config/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 cat ${CONFIG_PATH}/alertmanager-tmpl.yml \
-	| sed "s@\${SLACK_URL}@${SLACK_URL}@g" \
-	| sed "s@\${SLACK_CHANNEL}@${SLACK_CHANNEL}@g" \
-	| sed "s@\${SLACK_USER}@${SLACK_USER}@g" \
+	| sed "s@\${TELEGRAM_URL}@${TELEGRAM_URL}@g" \
+	| sed "s@\${TELEGRAM_CHAT_ID}@${TELEGRAM_CHAT_ID}@g" \
+	| sed "s@\${TELEGRAM_BOT_TOKEN}@${TELEGRAM_BOT_TOKEN}@g" \
+	| sed "s@\${TELEGRAM_PARSE_MODE}@${TELEGRAM_PARSE_MODE}@g" \
+	| sed "s@\${TELEGRAM_DISABLE_NOTIFICATIONS}@${TELEGRAM_DISABLE_NOTIFICATIONS}@g" \
 	> ${CONFIG_PATH}/alertmanager.yml
 
 set -- /bin/alertmanager "${@}"
